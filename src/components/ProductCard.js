29016a58126cffbedd09d9104d658e8089cd5e7c
mohammadtanim24h <mohammad.tanim24h@gmail.com>
2022-12-03 17:11:45 +0600

    added actionTypes and dispatched action using action creator

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,37 +1,50 @@
 import React from "react";
 import { BiListPlus } from "react-icons/bi";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../redux/actionCreators/productActions";
+import { ADD_TO_CART } from "../redux/actionTypes/actionTypes";
 
 const ProductCard = ({ product }) => {
-  return (
-    <div
-      className='shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900'
-      key={product._id}
-    >
-      <div className='h-52 w-52 mx-auto'>
-        <img src={product.image} alt={product.model} />
-      </div>
-      <h1 className='font-bold text-center'>{product.model}</h1>
-      <p className='text-center font-semibold mb-3'>Rating: {product.rating}</p>
-      <div className=' flex-1'>
-        <ul className='space-y-2'>
-          {product.keyFeature.map((feature, index) => {
-            return <li key={index} className='text-sm '>{feature}</li>;
-          })}
-        </ul>
-      </div>
-      <div className='flex gap-2 mt-5'>
-        <button className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'>
-          Add to cart
-        </button>
-        <button
-          title='Add to wishlist'
-          className='bg-indigo-500  py-1 px-2 rounded-full'
+    const dispatch = useDispatch();
+    return (
+        <div
+            className="shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900"
+            key={product._id}
         >
-          <BiListPlus className='text-white' />
-        </button>
-      </div>
-    </div>
-  );
+            <div className="h-52 w-52 mx-auto">
+                <img src={product.image} alt={product.model} />
+            </div>
+            <h1 className="font-bold text-center">{product.model}</h1>
+            <p className="text-center font-semibold mb-3">
+                Rating: {product.rating}
+            </p>
+            <div className=" flex-1">
+                <ul className="space-y-2">
+                    {product.keyFeature.map((feature, index) => {
+                        return (
+                            <li key={index} className="text-sm ">
+                                {feature}
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+            <div className="flex gap-2 mt-5">
+                <button
+                    className="bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold hover:bg-indigo-600 transition-all"
+                    onClick={() => dispatch(addToCart(product))}
+                >
+                    Add to cart
+                </button>
+                <button
+                    title="Add to wishlist"
+                    className="bg-indigo-500  py-1 px-2 rounded-full"
+                >
+                    <BiListPlus className="text-white" />
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default ProductCard;
