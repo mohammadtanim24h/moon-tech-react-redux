1a27be45560cea0613375d449d3d694d4bed9dbf
mohammadtanim24h <mohammad.tanim24h@gmail.com>
2022-12-03 19:57:32 +0600

    added remove from cart feature

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { BiListPlus } from "react-icons/bi";
 import { useDispatch } from "react-redux";
-import { addToCart } from "../redux/actionCreators/productActions";
-import { ADD_TO_CART } from "../redux/actionTypes/actionTypes";
+import {
+    addToCart,
+    removeFromCart,
+} from "../redux/actionCreators/productActions";
+import { AiFillDelete } from "react-icons/ai";
+import { useLocation } from "react-router-dom";
 
 const ProductCard = ({ product }) => {
     const dispatch = useDispatch();
+    const { pathname } = useLocation();
     return (
         <div
             className="shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900"
@@ -30,18 +35,32 @@ const ProductCard = ({ product }) => {
                 </ul>
             </div>
             <div className="flex gap-2 mt-5">
-                <button
-                    className="bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold hover:bg-indigo-600 transition-all"
-                    onClick={() => dispatch(addToCart(product))}
-                >
-                    Add to cart
-                </button>
-                <button
-                    title="Add to wishlist"
-                    className="bg-indigo-500  py-1 px-2 rounded-full"
-                >
-                    <BiListPlus className="text-white" />
-                </button>
+                {!pathname.includes("cart") && (
+                    <button
+                        className="bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold hover:bg-indigo-600 transition-all"
+                        onClick={() => dispatch(addToCart(product))}
+                    >
+                        Add to cart
+                    </button>
+                )}
+                {pathname.includes("cart") && (
+                    <button
+                        className={`bg-red-500 flex justify-center items-center rounded-full ${
+                            pathname.includes("cart") && "flex-1 text-center"
+                        } py-1 px-2 border text-bold text-white`}
+                        onClick={() => dispatch(removeFromCart(product))}
+                    >
+                        <span>Remove from cart</span> <AiFillDelete size={20}/>
+                    </button>
+                )}
+                {!pathname.includes("cart") && (
+                    <button
+                        title="Add to wishlist"
+                        className="bg-indigo-500  py-1 px-2 rounded-full"
+                    >
+                        <BiListPlus className="text-white" />
+                    </button>
+                )}
             </div>
         </div>
     );
